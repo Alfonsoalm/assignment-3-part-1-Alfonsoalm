7bb25670c4effb7b2cfa2ee08fb76cd7bfbede07
Alfonso Almenara <aalmenaral@oesia.com>
2026-01-13 13:44:54 +0100

    change finder-test form assignment 4 part 3

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -1,82 +1,76 @@
-
 #!/bin/sh
 # Tester script for assignment 1 and assignment 2
 # Author: Siddhant Jajoo
-# Modificado por Alfonso: usa la utilidad C 'writer' sin compilar aquí (A3 part 1)
-
+ 
 set -e
 set -u
-
+ 
 NUMFILES=10
 WRITESTR=AELD_IS_FUN
 WRITEDIR=/tmp/aeld-data
 username=$(cat conf/username.txt)
-
+ 
 if [ $# -lt 3 ]
 then
-    echo "Using default value ${WRITESTR} for string to write"
-    if [ $# -lt 1 ]
-    then
-        echo "Using default value ${NUMFILES} for number of files to write"
-    else
-        NUMFILES=$1
-    fi
+	echo "Using default value ${WRITESTR} for string to write"
+	if [ $# -lt 1 ]
+	then
+		echo "Using default value ${NUMFILES} for number of files to write"
+	else
+		NUMFILES=$1
+	fi	
 else
-    NUMFILES=$1
-    WRITESTR=$2
-    WRITEDIR=/tmp/aeld-data/$3
+	NUMFILES=$1
+	WRITESTR=$2
+	WRITEDIR=/tmp/aeld-data/$3
 fi
-
+ 
 MATCHSTR="The number of files are ${NUMFILES} and the number of matching lines are ${NUMFILES}"
-
+ 
 echo "Writing ${NUMFILES} files containing string ${WRITESTR} to ${WRITEDIR}"
-
-# Limpia el directorio de trabajo de datos previo
+ 
 rm -rf "${WRITEDIR}"
-
-# Detecta asignación actual
-assignment=$(cat conf/assignment.txt)
-
-# Si no es assignment1, crea el directorio (assignment2 lo requiere)
-if [ "${assignment}" != "assignment1" ]
+ 
+# create $WRITEDIR if not assignment1
+assignment=`cat ../conf/assignment.txt`
+ 
+if [ $assignment != 'assignment1' ]
 then
-    mkdir -p "${WRITEDIR}"
-    if [ -d "${WRITEDIR}" ]
-    then
-        echo "${WRITEDIR} created"
-    else
-        exit 1
-    fi
+	mkdir -p "$WRITEDIR"
+ 
+	#The WRITEDIR is in quotes because if the directory path consists of spaces, then variable substitution will consider it as multiple argument.
+	#The quotes signify that the entire string in WRITEDIR is a single string.
+	#This issue can also be resolved by using double square brackets i.e [[ ]] instead of using quotes.
+	if [ -d "$WRITEDIR" ]
+	then
+		echo "$WRITEDIR created"
+	else
+		exit 1
+	fi
 fi
-
-# *** IMPORTANTE: NO compilar en Assignment 3 Part 1 ***
-# Verificación suave de binario existente
-if [ ! -x "./writer" ]; then
-    echo "failed: writer binary not found or not executable. Build it separately (Part 2)."
-    exit 1
-fi
-
-# Escribir archivos usando la utilidad C 'writer'
-for i in $(seq 1 "${NUMFILES}")
+#echo "Removing the old writer utility and compiling as a native application"
+#make clean
+#make
+ 
+for i in $( seq 1 $NUMFILES)
 do
-    ./writer "${WRITEDIR}/${username}${i}.txt" "${WRITESTR}"
+	./writer.sh "$WRITEDIR/${username}$i.txt" "$WRITESTR"
 done
-
-# Ejecutar finder (script existente) para contar coincidencias
-OUTPUTSTRING=$(sh ./finder.sh "${WRITEDIR}" "${WRITESTR}")
-
+ 
+OUTPUTSTRING=$(./finder.sh "$WRITEDIR" "$WRITESTR")
+ 
 # remove temporary directories
 rm -rf /tmp/aeld-data
-
+ 
 set +e
-echo "${OUTPUTSTRING}" | grep "${MATCHSTR}" > /dev/null
+echo ${OUTPUTSTRING} | grep "${MATCHSTR}"
 if [ $? -eq 0 ]; then
-    echo "success"
-    exit 0
+	echo "success"
+	exit 0
 else
-    echo "failed: expected  ${MATCHSTR} in ${OUTPUTSTRING} but instead found"
-    exit 1
+	echo "failed: expected  ${MATCHSTR} in ${OUTPUTSTRING} but instead found"
+	exit 1
 fi
-
+ 
 # Al final del script
 echo "Final result" > /tmp/assignment4-result.txt
